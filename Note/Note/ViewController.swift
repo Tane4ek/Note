5eb493343c5fdc961f9a3644ed00204878921765
Tatyana Luzanova <82838416+Tane4ek@users.noreply.github.com>
2022-02-10 15:37:13 +0300

    add empty project

---
+++ Note/Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Поздняков Игорь Николаевич on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
